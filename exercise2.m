8a0ad4e8bec1bf77e2a288d89b1618ce019ecb7e
Juan R. Pacha <juan.ramon.pacha@upc.edu>
2020-03-09 10:34:13 +0100

    Give a more staright approach to exercise 2

--- exercise2.m
+++ exercise2.m
@@ -14,16 +14,10 @@ hold on
 
 [indNodBd,indElemBd,indLocalEdgeBd,edges]=boundaryNodes(nodes,elem);
 
-indNodLeft = find(nodes(:,1) < 0.01);
-indNodEq = setdiff(indNodBd,indNodLeft);
-indNodRight = find(nodes(:,1) > 5.99);
-indNodEq = setdiff(indNodEq,indNodRight);
-indNodBott = find(nodes(:,2) < 0.01);
-indNodEq = setdiff(indNodEq,indNodBott);
-indNodTopR = find(nodes(:,1) > 4.01);
-indNodEq = setdiff(indNodEq,indNodTopR);
-indNodTopL = find(nodes(:,1) < 1.99);
-indNodEq = setdiff(indNodEq,indNodTopL);
+indNodBottom = find(nodes(:,2) < 0.01);
+indNodEq = setdiff(indNodBd,indNodBottom);
+indNodRL = find(nodes(:,1) > 4.01 | nodes(:,1) < 1.99);
+indNodEq = setdiff(indNodEq,indNodRL);
 
 plot(nodes(indNodEq,1),nodes(indNodEq,2),'dm')
 hold off
